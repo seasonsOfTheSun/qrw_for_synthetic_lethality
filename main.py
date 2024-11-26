17620a0ded016e77369e911d70977910f4c669a5
“Joel <joel@menchelab.com>
2024-11-26 14:55:59 +0100

    prepare documentation in main for copying into README

--- main.py
+++ main.py
@@ -12,45 +12,47 @@ def main():
     
     folder = "data/intermediate/fourth_large"
     
-    n_modules,module_size,total_genes = 30,10,1000
-        
-    p_inter,p_intra = 0.02,1.0
-        
-        
-        
-    mutation_p = 0.2
-    n_patients = 200
-    
-    
     # here we create a simulation that models some features
-    # of real disease etitology
+    # of real disease etitology.
     
-    # we consider an arbitrary network genes, and some number of gene modules of constant size
-
-    n_modules,module_size,total_genes = 10,10,200
+    # we consider an arbitrary network genes, 
+    # and some number of gene modules of constant size
+    # n_modules,module_size,total_genes = 10,10,200
+    n_modules,module_size,total_genes = 30,10,1000
     gt = GroundTruth(n_modules,module_size,total_genes)
     gene_list = list(gt.genes)
+    
+    
+    # determines the characteristics of the 
+    # simulated PPI, specifically the probability
+    # of connection between nodes in a single gene module
+    # and 
+    # Creates a network on the genes, where the modules are especially dense and
+    # strongly connected.
+    p_inter,p_intra = 0.02,1.0
+    gt.ppi = PPI(gt,
+                p_inter,p_intra)
+    
+    pos = nx.spring_layout(gt.ppi.network)
 
 
- 
-
-    # we select two f the gene modules to be the disease modules 
+    # we select two of the gene modules to be the disease modules 
     # in each 'patient' these genes can be 'mutated' or not 'mutated'
     # when BOTH two disease modules have more mutations than some threshold (default 1)
     # we consider the 'patient' to have the disease
     gt.generate_disease_combinatorics()
 
-    
-
-    
-
-    all_patient_mutations = []
-    all_disease_statuses = []
-
     # here we simluate a population of patients who may or may not have the disease
     # by sampling uniformly over the genes with patient probab
     # ranfomly 'mutating' some of the genes and using the ground truth combinatorics
     # to determine if they have the disease or not
+
+    mutation_p = 0.2
+    n_patients = 200
+    
+    all_patient_mutations = []
+    all_disease_statuses = []
+
     for i in range(n_patients):
         patient_mutations = gt.simulate_mutations(mutation_p)
         patient_disease_status = gt.check_disease_status(patient_mutations)
@@ -59,30 +61,68 @@ def main():
         all_disease_statuses.append(patient_disease_status)
 
 
-    
+    # THIS IS THE POINT OF THE WHOLE MODULE!
+    # here we create an object, based on the patient data
+    # that calculates the behaviour 
+    # of a matrix that can hopefully be modified into 
+    # and interaction Hamiltonian.
     H = DataInteractionMatrix(gene_list,
                               all_patient_mutations,
                               all_disease_statuses
                               )
     
-    
 
-    gt.ppi = PPI(gt,
-                p_inter,p_intra)
-    
-
-    pos = nx.spring_layout(gt.ppi.network)
-
-
+    # Calculates the Symmetric-Normalized Adjacency Eigenvectors
     n_evecs = 10
     adjacency = nx.adjacency_matrix(gt.ppi.network,nodelist=gt.ppi.nodelist)
     evalues,evecs = calculate_evecs(adjacency,n_evecs
                                     )
 
-
     
     import os
     os.makedirs(folder,exist_ok=True)
+    
+    df = pd.DataFrame(evecs,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],
+                 index=gene_list)
+    df = df/np.linalg.norm(evecs,axis=0)
+    df.to_csv(f"{folder}/evecs.csv")
+    
+    # The inner products between the modules in the constructed matrix, 
+    # showing how the two disease modules (tensored together) have a much higher 
+    # value in <(module tensor module) | M |(module tensor module)> than the others
+    out=np.zeros((n_modules,n_modules))
+    for i,j in it.product(range(n_modules),repeat=2):
+        module_1,module_2 = gt.modules[i],gt.modules[j]
+        v = np.array([int(i in module_1) for i in gene_list])
+        w = np.array([int(i in module_2) for i in gene_list])
+        out[i,j] = H.bilinear_form_magnitude(v,w)
+        
+    df = pd.DataFrame(out,columns=[f"Module-{i}" for i in range(n_modules)],index=[f"Module-{i}" for i in range(n_modules)]) 
+    df.to_csv(f"{folder}/module_inner_products.csv")   
+    
+
+    out=np.zeros((n_evecs,n_evecs))
+    for i,j in it.product(range(n_evecs),repeat=2):
+
+        v=evecs[:,i]
+        w=evecs[:,j]
+
+        out[i,j] = H.bilinear_form_magnitude(v,w)
+    
+    df = pd.DataFrame(out,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],index=[f"Eigenvector-{i}" for i in range(n_evecs)]) 
+    df.to_csv(f"{folder}/eigenvector_inner_products.csv")   
+
+    # The inner products between the PPI evectors in the constructed matrix, 
+    # showing how the two PPI evectors representing the modules (tensored together) have a much higher 
+    # value in <(evec tensor evec) | M |(evec tensor evec)> than the others
+    out = []
+    for i in range(n_modules):
+        module = gt.modules[i]
+        v = np.array([int(i in module) for i in gene_list])
+        out.append(evecs.T @ v)
+    
+    df = pd.DataFrame(out,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],index=[f"Module-{i}" for i in range(n_modules)]).T
+    df.to_csv(f"{folder}/eigenvector_module_loadings.csv")   
     
     parameters = {"n_modules":n_modules,
                   "module_size":module_size,
@@ -106,42 +146,8 @@ def main():
     json.dump([[str(v) for v in w] for w in all_patient_mutations], open(f"{folder}/all_patient_mutations.json", 'w'))
     json.dump(all_disease_statuses, open(f"{folder}/all_disease_statuses.json", 'w'))
 
-    df = pd.DataFrame(evecs,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],
-                 index=gene_list)
-    df = df/np.linalg.norm(evecs,axis=0)
-    df.to_csv(f"{folder}/evecs.csv")
-    
-    out=np.zeros((n_modules,n_modules))
-    for i,j in it.product(range(n_modules),repeat=2):
-        module_1,module_2 = gt.modules[i],gt.modules[j]
-        v = np.array([int(i in module_1) for i in gene_list])
-        w = np.array([int(i in module_2) for i in gene_list])
-        out[i,j] = H.bilinear_form_magnitude(v,w)
-        
-    df = pd.DataFrame(out,columns=[f"Module-{i}" for i in range(n_modules)],index=[f"Module-{i}" for i in range(n_modules)]) 
-    df.to_csv(f"{folder}/module_inner_products.csv")   
-    
 
-    out=np.zeros((n_evecs,n_evecs))
-    for i,j in it.product(range(n_evecs),repeat=2):
 
-        v=evecs[:,i]
-        w=evecs[:,j]
-
-        out[i,j] = H.bilinear_form_magnitude(v,w)
-    
-    df = pd.DataFrame(out,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],index=[f"Eigenvector-{i}" for i in range(n_evecs)]) 
-    df.to_csv(f"{folder}/eigenvector_inner_products.csv")   
-    
-    out = []
-    for i in range(n_modules):
-        module = gt.modules[i]
-        v = np.array([int(i in module) for i in gene_list])
-        out.append(evecs.T @ v)
-    
-    df = pd.DataFrame(out,columns=[f"Eigenvector-{i}" for i in range(n_evecs)],index=[f"Module-{i}" for i in range(n_modules)]).T
-    df.to_csv(f"{folder}/eigenvector_module_loadings.csv")   
-    
     
 class PatientInteractionMatrix:
     
@@ -186,11 +192,10 @@ class DataInteractionMatrix:
     def __init__(self,nodelist,mutations,disease_statuses,patient_weighting_map=None):
         """ 
         Creates a matr}ix of the form
-        sum_{patients} sum_{ij} (|i> \tensor |j>)(<i| \tensor <j|)
+        sum_{patients} weight{patient} sum_{ij} (|i> \tensor |j>)(<i| \tensor <j|)
         where i,j are all pairs of genes in a particual patient,
-        summed over all the patients selected
-        
-        
+        summed over all the patients selected, and a weight is 
+        given according to whether the patirnt has the disease or not.
         """
         self.nodelist = nodelist
         
@@ -247,6 +252,13 @@ class GroundTruth:
         
         
     def generate_disease_combinatorics(self,threshold=1):
+        """ 
+        Randomly generates a choice of ground truth for the disease etiology model.
+        Randomly selecting a
+        pair of gene modules, henceforth the disease modules
+        and a threshold number of mutations that suffice for those 
+        modules to be considered inactivated. 
+        """
         
         i = np.random.choice(self.n_modules*(self.n_modules-1)//2)
         n,m = list(it.combinations(range(self.n_modules),2))[i]
@@ -265,6 +277,28 @@ class GroundTruth:
           
         
     def check_disease_status(self,mutations):
+        """ For the ground truth model held in this object,
+            would a given a set of mutations give rise to the disease?
+            
+            If interaction_type is "lethality", it checks that
+            both of the disease modules specified by the current ground truth 
+            contain at least 'threshold' number of mutations, and are therefore 
+            inactivated.
+            
+            If interaction_type is "rescue", it checks that one or the other disease module, 
+            but NOT both, contain at least 'threshold' number of mutations.
+            
+            note that by default 'threshold' is 1
+            
+            
+
+        Args:
+            mutations (list): a list of genes (nodes in the PPI network)
+            that describes the patients mutations.
+
+        Returns:
+            bool: according to the current disease model
+        """
         
         if self.interaction_type == "lethality":
             out = True
